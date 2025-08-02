55d25c9484aa242094f087d384e4a3c06810394b
JhonMestra21 <jhonmestrapc33@gmail.com>
2025-08-02 08:26:57 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+function filtrar(categoria){
+  let secciones = document.querySelectorAll('.seccion');
+  secciones.forEach(sec => {
+    if(categoria === 'todos'){
+      sec.style.display = 'block';
+    } else {
+      sec.style.display = sec.classList.contains(categoria) ? 'block' : 'none';
+    }
+  });
+}
+
+// Mostrar todos al inicio
+filtrar('todos');
+
+// Botón menú hamburguesa
+const menuBtn = document.getElementById('menu-btn');
+const menuNav = document.getElementById('menu-nav');
+
+menuBtn.addEventListener('click', () => {
+  menuNav.classList.toggle('show');
+});
+
